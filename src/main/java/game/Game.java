a0db17a77b592267257e00de6e649511912d3508
Filip Merlung <filip.merlung@gmail.com>
2022-10-22 14:30:25 +0200

    wip game class

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,5 +1,47 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game
 {
+    private Player player1;
+    private Player player2;
+    private Die die;
+    GameController controller;
+    public Game()
+    {
+        die = new Die();
+        player1 = new Player();
+        player2 = new Player();
+        controller = new GameController(die, player1, player2);
+    }
+    public void run()
+    {
+        initialize();
+        controller.startGameLoop();
+    }
+
+    public void initialize()
+    {
+        printWelcomeMessage();
+        getPlayerNames();
+        controller.startGameLoop();
+    }
+
+    private void getPlayerNames()
+    {
+        System.out.println("Please input name of player 1:\n");
+        Scanner scan = new Scanner(System.in);
+        this.player1.setName(scan.nextLine());
+        System.out.println();
+
+        System.out.println("Please input name of player 1:\n");
+        this.player2.setName(scan.nextLine());
+        System.out.println();
+    }
+
+    private void printWelcomeMessage()
+    {
+        System.out.println("Welcome to the game!");
+    }
 }
