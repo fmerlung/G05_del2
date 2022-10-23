a9203bbae106f37d3cfc55b5493c63b0690242e8
Filip Merlung <filip.merlung@gmail.com>
2022-10-23 13:24:07 +0200

    implementing gameloop and refactored most variables into class members

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -5,60 +5,68 @@ import java.util.Scanner;
 public class GameController
 {
     private Die die;
+    private int currentDiceRoll;
     private Player player1;
     private Player player2;
+    private Player currentPlayer = player1;
 
     public GameController(Die die, Player player1, Player player2)
     {
         this.die = die;
         this.player1 = player1;
         this.player2 = player2;
+        this.currentDiceRoll = 0;
+        this.currentPlayer = this.player1;
     }
 
     public void startGameLoop()
     {
-        int currentPlayer = 1;
-        int currentDiceRoll = 0;
         while(true)
         {
-            startRound(currentPlayer);
-            currentDiceRoll = rollDice();
+            requestInputToStart();
+            rollDice();
             updatePlayerBalance();
-            // print field flavour text
+            printFieldDescription();
             // evaluate game progress
-            updateCurrentPlayer(currentPlayer);
+            updateCurrentPlayer();
         }
+    }
+
+    private void printFieldDescription()
+    {
+        System.out.println(Board.getFieldDescription(this.currentDiceRoll));
     }
 
     private void updatePlayerBalance()
     {
+        this.currentPlayer.addToBalance(Board.getBalanceChange(this.currentDiceRoll));
     }
 
-    private void startRound(int currentPlayer)
+    private void requestInputToStart()
     {
         while (true)
         {
-            System.out.print("Player " + currentPlayer + ", please press 'Enter' to roll dice.");
+            System.out.print("Player " + this.currentPlayer.id + ", please press 'Enter' to roll dice.");
             Scanner scan = new Scanner(System.in);
-            String input = scan.next();
+            String input = scan.nextLine();
             if (input == "") break;
         }
     }
 
-    private int updateCurrentPlayer(int currentPlayer)
+    private void updateCurrentPlayer()
     {
-        if (currentPlayer < Player.numberOfPlayers)
+        if (this.currentPlayer.id = 1)
         {
-            currentPlayer += 1;
+            this.currentPlayer = this.player2;
         }
         else
         {
-            currentPlayer = 1;
+            this.currentPlayer = this.player1;
         }
     }
 
-    private int rollDice()
+    private void rollDice()
     {
-        return this.die.roll() + this.die.roll();
+        this.currentDiceRoll = this.die.roll() + this.die.roll();
     }
 }
