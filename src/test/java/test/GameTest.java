892f94bfe41297bf73903d683d28366085ee6a27
NovaSewerin <novasewerin@gmail.com>
2022-10-25 21:31:08 +0200

    Created test branch

--- src/test/java/test/GameTest.java
+++ src/test/java/test/GameTest.java
@@ -1,6 +1,19 @@
 package test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest
 {
-
+    @Test
+    @DisplayName("Testing to make sure that player balance can't be set to negative")
+    void changeBalanceBelowZeroTest()
+    {
+        Player testPlayer = new Player();
+        assertEquals(1000,testPlayer.getBalance());
+        testPlayer.changeBalance(-5000);
+        assertEquals(0,testPlayer.getBalance());
+    }
 }
