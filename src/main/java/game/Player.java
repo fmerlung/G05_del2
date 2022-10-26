b4f1f5c87fc966cb0f1541bc3a834a24ce29a0bd
Filip Merlung <filip.merlung@gmail.com>
2022-10-26 20:23:40 +0200

    added player turn id to determine turn order

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,9 +1,19 @@
 public class Player {
     private int balance;
 
+    private int turnOrderId;
+
     public Player() // Vores construktor
     {
         balance = 1000;
+    }
+
+    public int getTurnOrderId() {
+        return turnOrderId;
+    }
+
+    public void setTurnOrderId(int numberInTurnOrder) {
+        this.turnOrderId = numberInTurnOrder;
     }
 
     public int getBalance(){
